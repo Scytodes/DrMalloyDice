ad95bcd8ab963e4e45ae4471a57b957338250e0d
Scytodes <79763231+Scytodes@users.noreply.github.com>
2023-12-30 16:59:10 -0500

    Add files via upload
    Base

--- DrMalloy.js
+++ DrMalloy.js
@@ -2,8 +2,71 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addSystem({id: "DrMalloyDice", name: "Dr Malloy's Dice"}, false);
 	dice3d.addDicePreset({
 		type: "d2",
-		labels: "",
 		modelFile: "modules/DrMalloyDice/DrMalloyDice_d2.glb",
-		system: "DrMalloyDice"
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
 	});
+	dice3d.addDicePreset({
+		type: "dc",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d2.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d4",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d4.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d3",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d3.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d6",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d6.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d8",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d8.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+	
+		dice3d.addDicePreset({
+		type: "d12",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d12.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+
+		dice3d.addDicePreset({
+		type: "d10",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d10.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d100",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d100.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d5",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d5.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+		dice3d.addDicePreset({
+		type: "d20",
+		modelFile: "modules/DrMalloyDice/DrMalloyDice_d20.glb",
+		system: "DrMalloyDice",
+		emissive: "0x009d73"
+	});
+
 });
